b2c8f5d66dcbef21836c9d7d5ff79524ead54783
Roy Kirk <ricotalzier@gmail.com>
2016-12-07 10:18:51 +1100

    added beginning of troop command work

---
+++ Assets/scripts/Player/PlayerCommandingTroops.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCommandingTroops : MonoBehaviour {
+
+    public static GameObject[] troops;
+	// Use this for initialization
+	void Start () {
+        GameObject[] troopTemp = GameObject.FindGameObjectsWithTag("Troop");
+        troops = troopTemp;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/scripts/TroopCommands.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TroopCommands : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void FollowCommand()
+    {
+
+    }
+    public void AroundCommand()
+    {
+
+    }
+    public void RallyCommand()
+    {
+
+    }
+    public void AttackCommand()
+    {
+
+    }
+}
